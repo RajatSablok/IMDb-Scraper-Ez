f4708ab1d1638bbb348159a654f295ba9974771c
RajatSablok <rajat.main06@gmail.com>
2020-06-27 13:00:59 +0530

    most popular movies

--- api/routes/charts.js
+++ api/routes/charts.js
@@ -11,6 +11,7 @@ var ratingArr = [];
 var posterArr = [];
 var movieURLArr = [];
 var resultObj = [];
+var popularMoviesObj = [];
 
 //Get a list of top rated movies
 router.get("/topMovies", async (req, res, next) => {
@@ -66,6 +67,77 @@ router.get("/topMovies", async (req, res, next) => {
       res.status(200).json({
         message: "List successfully retrieved",
         result: resultObj,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Something went wrong",
+        error: err,
+      });
+    });
+});
+
+//Get a list of most popular movies
+router.get("/popularMovies", async (req, res, next) => {
+  var options = {
+    uri: "https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm",
+    transform: function (html) {
+      return cheerio.load(html);
+    },
+    headers: {
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-encoding": "gzip, deflate, br",
+      "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+    },
+  };
+
+  rp(options)
+    .then(($) => {
+      $(".titleColumn").each((i, el) => {
+        var title = $(el).find("a").text().trim();
+        titleArr[i] = title;
+
+        var year = $(el)
+          .find("span")
+          .text()
+          .replace(/[()]/g, "")
+          .trim()
+          .substring(0, 4);
+
+        yearArr[i] = year;
+
+        var movieURL = $(el).find("a").attr("href");
+        movieURLArr[i] = "https://www.imdb.com/" + movieURL;
+      });
+
+      $(".posterColumn ")
+        .find("a")
+        .each((i, el) => {
+          var poster = $(el).find("a img").attr("src");
+          posterArr[i] = poster;
+        });
+
+      $(".imdbRating ").each((i, el) => {
+        var rating = $(el).text().trim();
+        ratingArr[i] = rating;
+      });
+      // var YOR = JSON.parse(yearArr);
+      // console.log(YOR);
+      for (var i = 0; i < titleArr.length; i++) {
+        popularMoviesObj.push({
+          rank: i + 1,
+          title: titleArr[i],
+          yearOfRelease: yearArr[i],
+          rating: ratingArr[i],
+          posterURL: posterArr[i],
+          movieURL: movieURLArr[i],
+        });
+      }
+
+      res.status(200).json({
+        message: "List successfully retrieved",
+        result: popularMoviesObj,
       });
     })
     .catch((err) => {
