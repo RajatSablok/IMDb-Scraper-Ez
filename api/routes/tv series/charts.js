352d48a43ef8d0dc802094c5123d94802f70e9a5
RajatSablok <rajat.main06@gmail.com>
2020-06-27 13:40:56 +0530

    most popular tv shows

--- api/routes/tv series/charts.js
+++ api/routes/tv series/charts.js
@@ -77,4 +77,74 @@ router.get("/top", async (req, res, next) => {
     });
 });
 
+// Get a list of most popular tv shows
+router.get("/popular", async (req, res, next) => {
+  var options = {
+    uri: "https://www.imdb.com/chart/tvmeter/?ref_=nv_tvv_mptv",
+    transform: function (html) {
+      return cheerio.load(html);
+    },
+    headers: {
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-encoding": "gzip, deflate, br",
+      "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+    },
+  };
+
+  rp(options)
+    .then(($) => {
+      $(".titleColumn").each((i, el) => {
+        var title = $(el).find("a").text().trim();
+        titleArr[i] = title;
+
+        var year = $(el)
+          .find("span")
+          .text()
+          .replace(/[()]/g, "")
+          .trim()
+          .substring(0, 4);
+
+        yearArr[i] = year;
+
+        var tvSeriesURL = $(el).find("a").attr("href");
+        tvURLArr[i] = "https://www.imdb.com/" + tvSeriesURL;
+      });
+
+      $(".posterColumn ")
+        .find("a")
+        .each((i, el) => {
+          var poster = $(el).find("a img").attr("src");
+          posterArr[i] = poster;
+        });
+
+      $(".imdbRating ").each((i, el) => {
+        var rating = $(el).text().trim();
+        ratingArr[i] = rating;
+      });
+
+      for (var i = 0; i < titleArr.length; i++) {
+        popularMoviesObj.push({
+          rank: i + 1,
+          title: titleArr[i],
+          yearOfRelease: yearArr[i],
+          rating: ratingArr[i],
+          posterURL: posterArr[i],
+          tvSeriesURL: tvURLArr[i],
+        });
+      }
+
+      res.status(200).json({
+        message: "List successfully retrieved",
+        result: popularMoviesObj,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Something went wrong",
+        error: err,
+      });
+    });
+});
+
 module.exports = router;
